fb7005e7154500ebb4c24dba7eeeea07ab7283a7
AvaRetail <146922607+AvaRetail@users.noreply.github.com>
2024-02-15 16:29:46 +0000

    logo updated as per the dimensions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0" data-aos="fade-down" data-aos-duration="4500">
         <a href="index.html" className="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
             {/* <h2 className="m-0 text-primary">Droit Energy</h2> */}
-            <img src={logo}/>
+            <img src={logo} height="75px" width="185"/>
 
         </a>
         <button type="button" className="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
